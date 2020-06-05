1908dca8db48f26e1b0f014b8d20a116a5449515
Aryan Mukerji <aryanmukerji@gmail.com>
2020-06-05 15:03:07 +0530

    Stack using Classes in C++

--- StackClass.cpp
+++ StackClass.cpp
@@ -24,3 +24,37 @@ class Stack
     void Pop();
     void Display();
 };
+
+void Stack::Push()
+{
+    City *Temp;
+    
+    Temp = new City;
+    
+    if(Temp == NULL)
+    {
+        cout<<"\n Node Not Able To Create !!! \n";
+        return;
+    }
+    
+    cout<<"\n Enter City Details :- ";
+    
+    cout<<"\n Enter the City Pin Code : ";
+    cin>>Temp -> Pin;
+    
+    cout<<"\n Enter the City Name : ";
+    cin>>Temp -> Name;
+    
+    Temp -> Next = NULL;
+    
+    if(Top == NULL)
+    {
+		Top = Temp;
+    }	
+	else
+	{
+		Temp -> Next = Top;
+		
+		Top = Temp;
+	}
+}
